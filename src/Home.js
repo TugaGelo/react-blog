f00db3878685d5ddc711660861b700fe1b1034ba
Tugaga <rolandtugaoen013@gmail.com>
2025-04-04 21:47:50 +0800

    feat: add async data fetching with loading indicators

--- src/Home.js
+++ src/Home.js
@@ -2,44 +2,28 @@ import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-const [blogs, setBlogs] = useState([
-    {
-        id: 1,
-        title: 'Getting Started with React',
-        body: 'React is a JavaScript library for building user interfaces...',
-        author: 'Mario'
-    },
-    {
-        id: 2,
-        title: 'Understanding useState Hook',
-        body: 'The useState hook lets you add state to functional components...',
-        author: 'Luigi'
-    },
-    {
-        id: 3,
-        title: 'The Power of Components',
-        body: 'Components let you split the UI into independent, reusable pieces...',
-        author: 'Peach'
-    },
-    {
-        id: 4,
-        title: 'React Router Basics',
-        body: 'React Router enables navigation between views in a React app...',
-        author: 'Mario'
-    }
-]);
+const [blogs, setBlogs] = useState(null);
+const [isPending, setIsPending] = useState(true)
 
     const [name, setName] = useState('mario');
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
 
     useEffect(() => {
-        console.log('use effect ran');
-        console.log({name});
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsPending(false);
+            });
+        // console.log('use effect ran');
+        // // console.log({name});
+    }, []);
 
     // // let name = 'mario';
 
@@ -59,15 +43,21 @@ const [blogs, setBlogs] = useState([
 
     return ( 
         <div className="Home">
+            { isPending && <div>Loading...</div>}
             {/* <h2>Home Page</h2>
+ 
             <p>{name} is { age } years old</p>
+ 
             <button onClick={handleClick}>Click Me</button>
+ 
             <button onClick={() => {handleClickAgain('stephanie')}}>Click Me Again</button> */}
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter(blog => blog.author === 'Mario')} title="Mario's Blogs" handleDelete={handleDelete}/>
+
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+
+            {/* <BlogList blogs={blogs.filter(blog => blog.author === 'Mario')} title="Mario's Blogs" handleDelete={handleDelete}/> */}
             
-            <button onClick={() => setName('luigi')}>Change Name</button>
-            <p>{ name }</p>
+            {/* <button onClick={() => setName('luigi')}>Change Name</button>
+            <p>{ name }</p> */}
 
         </div>
      );
