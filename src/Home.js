bc8456a9df5d15c7017794464633f75df1ce600b
Tugaga <rolandtugaoen013@gmail.com>
2025-04-04 21:57:53 +0800

    feat: add error handling for fetching data

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,8 @@ import BlogList from './BlogList';
 
 const Home = () => {
 const [blogs, setBlogs] = useState(null);
-const [isPending, setIsPending] = useState(true)
+const [isPending, setIsPending] = useState(true);
+const [error, setError] = useState(null);
 
     const [name, setName] = useState('mario');
 
@@ -15,10 +16,18 @@ const [isPending, setIsPending] = useState(true)
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then((data) => {
                 setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setError(err.message);
                 setIsPending(false);
             });
         // console.log('use effect ran');
@@ -43,6 +52,7 @@ const [isPending, setIsPending] = useState(true)
 
     return ( 
         <div className="Home">
+            {error && <div>{error}</div>}
             { isPending && <div>Loading...</div>}
             {/* <h2>Home Page</h2>
  
